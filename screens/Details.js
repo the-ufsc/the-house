201052d71d60aadbffc0c1361b26b671272cf11b
jeffersonreis <jeffersonluis.reis@gmail.com>
2022-04-29 22:29:45 -0300

    detail

--- screens/Details.js
+++ screens/Details.js
@@ -4,8 +4,16 @@ import { StyleSheet, View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function DetailsScreen(props) {
-  // let house = props.navigation.getParam("house");
-  // console.log("a", house);
+  const house = {
+    name: "Moradia 01",
+    address: "Rua das Pedras, 58",
+    price: "680,00",
+    accommodations: 5,
+    images: [
+      "https://www.imovelweb.com.br/noticias/wp-content/uploads/2020/03/original-f6761ad4df0a26d3ed2cdf9d7d34d893.jpeg",
+    ],
+    gender: "man",
+  };
 
   return (
     <SafeAreaView>
