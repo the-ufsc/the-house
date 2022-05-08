5d23cb7e1dddaea7875ee04d713336f147651604
jeffersonreis <jeffersonluis.reis@gmail.com>
2022-05-08 14:34:41 -0300

    fixed json

--- components/Filter.js
+++ components/Filter.js
@@ -1,32 +1,9 @@
-import RangeSlider from "rn-range-slider";
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  Image,
-  ScrollView,
-  TouchableOpacity,
-  Linking,
-  TextInput,
-} from "react-native";
+import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React, { useState, useCallback } from "react";
-import { SliderPicker } from "react-native-slider-picker/src/components/SliderPicker";
 
 export default function Filter() {
   const [open, setOpen] = useState(false);
-  const [min, setMin] = useState(0);
-
-  const renderThumb = useCallback(() => <Thumb />, []);
-  const renderRail = useCallback(() => <Rail />, []);
-  const renderRailSelected = useCallback(() => <RailSelected />, []);
-  const renderLabel = useCallback((value) => <Label text={value} />, []);
-  const renderNotch = useCallback(() => <Notch />, []);
-  const handleValueChange = useCallback((low, high) => {
-    setLow(low);
-    setHigh(high);
-  }, []);
 
   return (
     <View style={styles.container}>
@@ -58,44 +35,6 @@ export default function Filter() {
           <View style={styles.container}>
             <View style={styles.box}>
               <Text>Valor Mínimo:</Text>
-              {/* <Text>{min}</Text> */}
-              {/* <SliderPicker
-                minLabel={"0"}
-                maxLabel={"100"}
-                maxValue={100}
-                callback={(position) => {
-                  setMin(position);
-                }}
-                defaultValue={min}
-                labelFontColor={"#6c7682"}
-                labelFontWeight={"600"}
-                showFill={true}
-                fillColor={"green"}
-                // labelFontWeight={'bold'}
-                showNumberScale={true}
-                showSeparatorScale={true}
-                buttonBackgroundColor={"#fff"}
-                buttonBorderColor={"#6c7682"}
-                buttonBorderWidth={1}
-                scaleNumberFontWeight={"200"}
-                buttonDimensionsPercentage={6}
-                heightPercentage={1}
-                widthPercentage={80}
-              /> */}
-
-              <Slider
-                style={styles.slider}
-                min={0}
-                max={100}
-                step={1}
-                floatingLabel
-                renderThumb={renderThumb}
-                renderRail={renderRail}
-                renderRailSelected={renderRailSelected}
-                renderLabel={renderLabel}
-                renderNotch={renderNotch}
-                onValueChanged={handleValueChange}
-              />
             </View>
           </View>
         </View>
--- screens/Home.js
+++ screens/Home.js
@@ -1,24 +1,39 @@
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import React from "react";
-import { ScrollView, StyleSheet, View, Text, Image } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
 import { Card } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
 import Filter from "../components/Filter";
-// import ButtonsMenu from "../components/ButtonsMenu";
 import homes from "../database/homes.json";
 
 export default function HomeScreen({ props, navigation }) {
   const houses = homes;
 
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(10000);
+  const [gender, setGender] = useState("any");
+
+  function verifyFilter(house) {
+    if (house.min >= min && house.max <= max && (gender === "any" || house.gender === gender)) {
+      return house;
+    }
+  }
+
+  function testFilter() {
+    console.log(houses.filter(verify));
+  }
+
   return (
     <View style={styles.container}>
       <ScrollView>
         <Filter />
+        <TouchableOpacity onPress={() => testFilter()}>
+          <Text>CLICK ME</Text>
+        </TouchableOpacity>
         {houses?.map((house, index) => (
           <Card
             key={"house-" + index}
-            onPress={() => navigation.navigate("Detalhes", {house: house})}
+            onPress={() => navigation.navigate("Detalhes", { house: house })}
             style={styles.card}
           >
             <Card.Title title={house.name} />
@@ -33,7 +48,7 @@ export default function HomeScreen({ props, navigation }) {
             <View style={styles.infoBox}>
               <View style={styles.boxCard}>
                 <Text>Endereço: {house.address}</Text>
-                <Text>R$ {house.price}</Text>
+                <Text>R$ {house.price.toString()}</Text>
                 <Text>{house.accommodations} alojamentos</Text>
               </View>
 
